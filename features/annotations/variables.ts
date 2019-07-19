337f5efb8879937009d8c0078f9f5d8aea1b67cd
Mario Lazzari <mario.lazzari@gmail.com>
2019-07-19 22:31:56 +0200

    Done lesson 15

---
+++ features/annotations/variables.ts
@@ -0,0 +1,24 @@
+let apple: number = 5;
+let string: string = "fast";
+let hasName: boolean = true;
+
+let notMuch: null = null;
+let nothing: undefined = undefined;
+
+// buuilt in obj
+let now: Date = new Date();
+
+// array
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let thruts: boolean[] = [true, true, false];
+
+// classes
+class Car {}
+let car: Car = new Car();
+
+// obj litteral
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
