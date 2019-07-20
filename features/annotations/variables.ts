ebce527c9dd0d282ec5d0dafd7c4a07cff231a33
Mario Lazzari <mario.lazzari@gmail.com>
2019-07-20 16:41:56 +0200

    Done lesson 26

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -22,3 +22,32 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20
 };
+
+// functions
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// any type
+const json = '{"x":10,"y":20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// delaued init
+let words = ["red", "green", "blus"];
+let found: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "red") {
+    found = true;
+  }
+}
+
+// inferred not possible
+let numbers = [-1, 1 - 11];
+let numberAboveZero: boolean | number = false;
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] < 0) {
+    numberAboveZero = numbers[i];
+  }
+}
