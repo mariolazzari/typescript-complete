0b9910799d8cc41b16531d13aeecdca47c85e58d
Mario Lazzari <mario.lazzari@gmail.com>
2019-07-23 20:58:35 +0200

    Done lesson 59

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,13 @@
-console.log("index.ts");
+import { User } from "./User";
+import { Company } from "./Company";
+
+const user = new User();
+console.log(user);
+
+const company = new Company();
+console.log(company);
+
+const map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: { lat: 0, lng: 0 }
+});
