9fb156cbf46996e06ad17e688be080449c838ab5
Mario Lazzari <mario.lazzari@gmail.com>
2019-07-25 21:33:15 +0200

    Done lesson 84

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,25 +1,13 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-  sort(): void {
-    const { length } = this.collection;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const temp = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = temp;
-          }
-        }
-
-        if (typeof this.collection === "string") {
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, -3, 0, 5]);
+const numersCollection = new NumbersCollection([10, -3, 0, 5]);
+let sorter = new Sorter(numersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numersCollection.data);
+
+const chars = new CharactersCollection("mario");
+sorter = new Sorter(chars);
+sorter.sort();
+console.log(chars.data);
